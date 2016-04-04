18d6bdbac44f0040b90b85465136fe602cd6eecc
Emiliano Suárez <emilianosuarez@gmail.com>
2016-04-04 20:34:37 -0300

    Fix obteniendo los elementos de la matriz. Implementacion de la funcion 'matrix_multiply'

--- tp0/src/main.c
+++ tp0/src/main.c
@@ -23,8 +23,14 @@ int print_matrix(FILE* fp, matrix_t* m);
 // Multiplica las matrices en m1 y m2
 matrix_t* matrix_multiply(matrix_t* m1, matrix_t* m2);
 
+/*
+Lee una línea por stdin y devuelve los elementos de la matriz y la
+dimensión por referencia.
+*/
+double* read_arguments(char* line, int line_size, int *matrix_dimension);
 
-int read_arguments(char* line, int line_size, double* matrix_elements);
+// Devuele la cantidad total de elementos de ambas matrices.
+int get_amount_element(int dimension);
 
 int main (int argc, char *argv[])
 {
@@ -35,7 +41,12 @@ int main (int argc, char *argv[])
     int matrix_dimension = 0;
     int amount_elements = 0;
     double matrix_elements = 0;
+    double* matrix_elements_pointer;
     double* element_pointer = 0;
+    double* pointer_m1 = 0;
+    double* pointer_m2 = 0;
+    matrix_t* m1;
+    matrix_t* m2;
 
     // Leo desde stdin
     while( (c = getc(stdin)) != EOF)
@@ -52,16 +63,19 @@ int main (int argc, char *argv[])
 
         // Si finalizó la línea, multiplico las matrices
         if ('\n' == c) {
-            matrix_dimension = read_arguments(line, chars_per_line,
-                                              &matrix_elements);
+            matrix_elements_pointer = read_arguments(line, chars_per_line,
+                                              &matrix_dimension);
+
+            element_pointer = matrix_elements_pointer;
 
             // Calculo la cantidad de elementos de ambas matrices.
-            amount_elements = matrix_dimension * matrix_dimension * 2;
+            amount_elements = get_amount_element(matrix_dimension);
 
             printf("matrix_dimension: %d\n", matrix_dimension);
             printf("amount_elements: %d\n", amount_elements);
 
-            element_pointer = &matrix_elements;
+            // m1 = create_matrix(matrix_dimension, matrix_dimension);
+            // m1->array = element_pointer;
 
             i = 0;
             do {
@@ -69,6 +83,9 @@ int main (int argc, char *argv[])
                 i++;
                 element_pointer++;
             } while(i < amount_elements);
+
+            // printf("m1.col: %d\n", m1->cols);
+            // printf("m1.rows: %d\n", m1->rows);
 
             chars_per_line = 0;
         }
@@ -107,27 +124,49 @@ int print_matrix(FILE* fp, matrix_t* m)
     return 0;
 }
 
-// Multiplica las matrices en m1 y m2
 matrix_t* matrix_multiply(matrix_t* m1, matrix_t* m2)
 {
-    matrix_t* result = malloc(sizeof(matrix_t));
+    matrix_t* result = create_matrix(m1->rows, m1->cols);
+    int dim = m1->rows;
+    int i,j,k,x = 0;
+
+    for(; k < (dim * dim) ; i += dim){
+
+        for(; x < dim; j++, k++, x++){
+
+            int temp = 0;
+
+            for (;j < (dim * dim); i++, j += dim){
+                temp = temp + m1->array[i] * m2->array[j];
+            }
+
+            j -= (dim * dim);
+            i -= dim;
+            result->array[k] = temp;
+        }
+        x = 0;
+        j = 0;
+    }
 
     return result;
 }
 
-int read_arguments(char* line, int line_size, double* matrix_elements) {
-
+double* read_arguments(char* line, int line_size, int *matrix_dimension) {
     char *first_token;
     char *search = " ";
     double element = 0;
-    int dimension = 0;
+    double* matrix_elements;
+    double* pointer_to_first_element;
+
+    matrix_elements = malloc(sizeof(double));
+    pointer_to_first_element = matrix_elements;
 
     /*
      Leo la dimensión de la matriz, que está en la primer posición
      de la línea.
     */
     first_token = strtok(line, search);
-    dimension = atoi(first_token);
+    *matrix_dimension = atoi(first_token);
 
     while ( (first_token = strtok( NULL, search)) != NULL) {
         element = atof(first_token);
@@ -136,5 +175,15 @@ int read_arguments(char* line, int line_size, double* matrix_elements) {
         matrix_elements++;
     }
 
-    return dimension;
+    // Seteo el fin del array
+    *matrix_elements = '\0';
+
+    matrix_elements = pointer_to_first_element;
+
+    return matrix_elements;
+}
+
+int get_amount_element(int dimension)
+{
+    return dimension * dimension * 2;
 }
